5eae6ad717168e09d01c355e830828a38009861e
dasGitto <mafyvic@gmail.com>
2025-01-19 14:05:12 +0100

    TO FAST LED C3PALMFLOWER.ino

--- C3PALMFLOWER.ino
+++ C3PALMFLOWER.ino
@@ -1,32 +1,34 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 #include <Arduino.h>
 
 #define BUTTON_PIN D0 // Pin for the button
 #define PIN D7        // Pin for the LED strip
-#define NUM_LEDS 16  // Number of LEDs in the strip
+#define NUM_LEDS 16   // Number of LEDs in the strip
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+// Declare the LED array using FastLED
+CRGB leds[NUM_LEDS];  
 
 extern void handleButtonPress(); // Declare the function from another file
 extern void updateLEDStrip();    // Declare the update function from another file
 extern void IRAM_ATTR buttonISR();
 
 void setup() {
-    // initial serial communication at 115200 baud
+    // Initial serial communication at 115200 baud
     Serial.begin(115200);
     delay(100);
-    Serial.println ("Serial monitor initialized");
+    Serial.println("Serial monitor initialized");
 
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISR, CHANGE);
 
-    strip.begin();  // Initialize the NeoPixel strip
-    strip.show();   // Set all pixels to 'off'
+    FastLED.addLeds<WS2812, PIN, GRB>(leds, NUM_LEDS);  // Initialize the FastLED strip
+    FastLED.clear();  // Turn off all LEDs initially
+    FastLED.show();
 
     // Check if waking up from deep sleep
     if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0) {
         updateLEDStrip(); // Restore the last LED state
-        Serial.println("Wakeup from sleep");
+        Serial.println("Wakeup from sleep");`
     }
 }
 
